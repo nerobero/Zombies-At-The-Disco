d27ad1e0bc286f608b3eb505619b753f3b004aac
BuildTools <unconfigured@null.spigotmc.org>
2022-10-20 14:44:15 -0400

    bat in hand

--- Assets/Scripts/Weapon System/WeaponScript.cs
+++ Assets/Scripts/Weapon System/WeaponScript.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
-    
+
+    void attack()
+    {
+        // Vector3 distanceToPlayer = player.position - transform.position;
+        // if (!equipped && distanceToPlayer.magnitude <= pickupRange && Input.GetKeyDown(KeyCode.E) )
+        // {
+        //     PickUp();
+        // }
+    }
     // Start is called before the first frame update
     void Start()
     {
