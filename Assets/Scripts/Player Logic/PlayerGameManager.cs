0fa005854f9c847ab2a8d555853bf830ece22645
Ji Yoon Rhee <angelarhee02@gmail.com>
2022-12-01 14:44:59 -0500

    Update PlayerGameManager.cs

--- Assets/Scripts/Player Logic/PlayerGameManager.cs
+++ Assets/Scripts/Player Logic/PlayerGameManager.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerGameManager : MonoBehaviour
 {
+    private bool gameEnded = false;
+    public float respawnTime = 2f;
+    
     public void EndGameRound()
     {
-        Debug.Log("The player has died!");
+        if (!gameEnded)
+        {
+            gameEnded = true;
+            Debug.Log("The player has died!");
+            //restart game here
+            Invoke("RestartRound", respawnTime);
+        }
+    }
+
+    void RestartRound()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
