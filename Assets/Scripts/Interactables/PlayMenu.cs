1053677c60600e3c3ff287548aa5fb26f3f902e8
Ji Yoon Rhee <angelarhee02@gmail.com>
2022-12-04 01:32:49 -0500

    f

--- Assets/Scripts/Interactables/PlayMenu.cs
+++ Assets/Scripts/Interactables/PlayMenu.cs
@@ -7,7 +7,7 @@ public class PlayMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Restart()
