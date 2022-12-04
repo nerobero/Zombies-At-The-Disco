6b7bc8822a2e3acfc80556bd3da8c449cac3f3c9
QiQi1666 <99517531+QiQi1666@users.noreply.github.com>
2022-12-04 18:29:46 -0500

    One super quick thing

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -5,10 +5,12 @@ using UnityEngine.UIElements;
 
 public class GUIManager : MonoBehaviour
 {
+    private GameObject Bryce;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Bryce = GameObject.Find("Bryce");
     }
 
     // Update is called once per frame
