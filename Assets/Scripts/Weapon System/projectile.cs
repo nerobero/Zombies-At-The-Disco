fc1adf33032666aff4483ef26d34fc7686b5725a
Ji Yoon Rhee <angelarhee02@gmail.com>
2022-12-04 18:12:17 -0500

    Revert "Gui changes"
    This reverts commit f8187d0294dfc531c601678c75c57444ef42fe10.

--- Assets/Scripts/Weapon System/projectile.cs
+++ Assets/Scripts/Weapon System/projectile.cs
@@ -1,56 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-//
-// public class projectile : MonoBehaviour
-// {
-//     [Header("References")]
-//     public Transform cam;
-//     public Transform attackPoint;
-//     public Rigidbody rigid;
-//     
-//     [Header("Settings")] 
-//     public float throwForce;
-//     public float throwUpwardForce;
-//     
-//
-//     
-//     private bool equipped = false;
-//     
-//     
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         
-//     }
-//     
-//     void PickUp()
-//     {
-//         equipped = true;
-//         rigid.isKinematic = true;
-//         // collider.isTrigger = true;
-//         
-//         transform.SetParent(attackPoint);
-//         transform.localPosition = Vector3.zero;
-//         transform.localRotation = Quaternion.Euler(Vector3.zero);
-//         // transform.localScale = Vector3.one;
-//         ;
-//
-//         readyToThrow = true;
-//         playerController.weaponScript = weapon;
-//     }
-//
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         
-//     }
-// }
-=======
->>>>>>> Stashed changes
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -78,7 +25,3 @@ public class Projectile : MonoBehaviour
         }
     }
 }
-<<<<<<< Updated upstream
-=======
->>>>>>> 0295ca35ba3e68ce4a31298fd6f3fb29f77ca3c8
->>>>>>> Stashed changes
