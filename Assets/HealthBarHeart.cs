23e34cf9c16de77329099eabde4c7423892029c3
WhaleoWhale <RMLewis20@gmail.com>
2022-12-04 19:13:01 -0500

    Sunday night

---
+++ Assets/HealthBarHeart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHeart : MonoBehaviour
+{
+    GameObject
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentHealth = (int)playerObj.getComponent<HPSystem>().currentHealth;
+        gameObject.GetComponent<Image>.fillAmount = currentHealth;
+        
+    }
+}
