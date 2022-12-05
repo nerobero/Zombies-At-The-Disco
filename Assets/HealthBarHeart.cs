1db23ff26fc058f32ad4b97359979273dfa37d55
QiQi1666 <99517531+QiQi1666@users.noreply.github.com>
2022-12-04 20:11:53 -0500

    HEART CHANGES

--- Assets/HealthBarHeart.cs
+++ Assets/HealthBarHeart.cs
@@ -5,18 +5,20 @@ using UnityEngine.UI;
 
 public class HealthBarHeart : MonoBehaviour
 {
-    GameObject
+    private GameObject playerObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerObj = GameObject.Find("Bryce");
     }
 
     // Update is called once per frame
     void Update()
     {
-        int currentHealth = (int)playerObj.getComponent<HPSystem>().currentHealth;
-        gameObject.GetComponent<Image>.fillAmount = currentHealth;
-        
+        float currentHealth = playerObj.GetComponent<HPSystem>().currentHealth;
+        float maxHealth = playerObj.GetComponent<HPSystem>().maxHealth;
+        gameObject.GetComponent<Image>().fillAmount = currentHealth / maxHealth;
+
     }
 }
