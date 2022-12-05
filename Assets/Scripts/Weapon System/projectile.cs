d8f99b37e41eba68f7865dbbcae66ec414c9ec03
BuildTools <unconfigured@null.spigotmc.org>
2022-12-04 19:15:42 -0500

    tthrow with c

--- Assets/Scripts/Weapon System/projectile.cs
+++ Assets/Scripts/Weapon System/projectile.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zombie_Logic;
 
 public class Projectile : MonoBehaviour
 {
 
+    public float damage;
     private Rigidbody rb;
 
-    private bool targetHit;
+    private bool targetHit = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +23,10 @@ public class Projectile : MonoBehaviour
     {
         if (!targetHit)
         {
-            targetHit = true;
-            Destroy(gameObject);
+            if (collision.gameObject.GetComponent<AbstractZombieAI>() != null)
+            {
+                collision.gameObject.GetComponent<AbstractZombieAI>().TakeDamage(damage);
+            }
         }
     }
 }
--- Assets/objectthrower.cs
+++ Assets/objectthrower.cs
@@ -28,7 +28,7 @@ public class objectthrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && readyToThrow)
+        if (Input.GetKeyDown(KeyCode.C) && readyToThrow)
         {
             Throw();
         }
